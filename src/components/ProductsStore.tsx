b1c40de385473412a51ebcc2fb0c6379f6797abb
eeddee <edward@edwardsharp.net>
2019-09-14 19:37:29 -0400

    use zee productMapFn :zap:

--- src/components/ProductsStore.tsx
+++ src/components/ProductsStore.tsx
@@ -5,6 +5,7 @@ import { Product } from './Product'
 import { Service } from '../types/Service'
 import { ProductDoc } from '../types/Product'
 import Loading from './Loading'
+import { productMapFn } from '../util/utilz'
 
 interface ProductsStoreProps {
   selectedCat: string
@@ -13,22 +14,17 @@ interface ProductsStoreProps {
 
 function ProductsStore(props: ProductsStoreProps) {
   const { selectedCat, productDocResult } = props
+  const productMap =
+    productDocResult.status === 'loaded'
+      ? productDocResult.payload.product_map
+      : undefined
 
-  // #TODO: use a generated product map...
   const rows =
     productDocResult.status === 'loaded' && productDocResult.payload.data
-      ? productDocResult.payload.data
-          .filter(product => product[10] === selectedCat)
-          .map((prod: string[]) => [
-            prod[2].trim(),
-            prod[3].trim(),
-            prod[4],
-            prod[5],
-            prod[6],
-            prod[8],
-            prod[9],
-            ...prod.slice(11, prod.length)
-          ])
+      ? productDocResult.payload.data.filter(
+          product =>
+            productMapFn('category', product, productMap) === selectedCat
+        )
       : undefined
 
   return (
@@ -40,12 +36,12 @@ function ProductsStore(props: ProductsStoreProps) {
             <Loading />
           </Box>
         )}
-        // scrollableAncestor="window"
         items={rows}
-        // onMore={() => console.log('infinite onMore!')}
         step={100}
       >
-        {(row, i) => <Product row={row} key={`infpro${i}`} />}
+        {(row, i) => (
+          <Product row={row} productMap={productMap} key={`infpro${i}`} />
+        )}
       </InfiniteScroll>
     </Box>
   )
